2444d26ff1786ee36db62c0410e564d62a92eeb8
Werlei Mendes Andrade <werlleymendes@hotmail.com>
2024-09-03 07:37:39 -0300

    add filtros de categoria, clausula group by e order by

--- Venda de vinho Melhores Clientes.sql
+++ Venda de vinho Melhores Clientes.sql
@@ -1,6 +1,6 @@
-/*SELECT * FROM consinco.MAXV_ABCDISTRIBBASE;*/
+SELECT * FROM consinco.MAXV_ABCDISTRIBBASE;
 
-SELECT a.seqpessoa, d.nomerazao, d.fonenro1
+SELECT b.seqproduto, b.desccompleta, sum(a.qtditem)
        FROM consinco.maxv_abcdistribbase a
        JOIN consinco.map_produto b
             ON a.seqproduto = b.seqproduto
@@ -8,7 +8,21 @@ SELECT a.seqpessoa, d.nomerazao, d.fonenro1
             ON b.seqfamilia = c.seqfamilia
        JOIN consinco.ge_pessoa d
             ON a.seqpessoa = d.seqpessoa
+       JOIN consinco.map_famdivcateg e
+            ON c.seqfamilia = e.seqfamilia
+       JOIN consinco.map_categoria f
+            ON e.seqcategoria = f.seqcategoria
        WHERE a.dtavda BETWEEN '01-aug-2023' and '31-aug-2024' AND
-             a.vlritem <= 20 AND
+             f.seqcategoria IN (select g.seqcategoria from consinco.map_categoria g
+                                       where g.categoria LIKE 'VINHO%' and
+                                       g.nivelhierarquia = 4
+                               ) and
+             f.statuscategor = 'A' and
+             e.status = 'A' and
+             DISTINCT(a.seqitem)
+       GROUP BY b.seqproduto, b.desccompleta
+       ORDER BY 3;
              
              
+SELECT * FROM consinco.map_categoria o where o.categoria LIKE 'VINHO%' AND O.STATUSCATEGOR = 'A'
+SELECT * FROM consinco.map_categoria o where o.seqcategoria in (2013, 2870);
