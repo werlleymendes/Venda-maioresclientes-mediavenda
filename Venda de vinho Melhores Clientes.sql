fc101718178c5d0de8771d9305216fd5561fe61f
Werlei Mendes Andrade <werlleymendes@hotmail.com>
2024-09-02 21:05:51 -0300

    Add git ignore e criando filtros iniciais da query

--- Venda de vinho Melhores Clientes.sql
+++ Venda de vinho Melhores Clientes.sql
@@ -1,6 +1,6 @@
 /*SELECT * FROM consinco.MAXV_ABCDISTRIBBASE;*/
 
-SELECT * 
+SELECT a.seqpessoa, d.nomerazao, d.fonenro1
        FROM consinco.maxv_abcdistribbase a
        JOIN consinco.map_produto b
             ON a.seqproduto = b.seqproduto
@@ -8,3 +8,7 @@ SELECT *
             ON b.seqfamilia = c.seqfamilia
        JOIN consinco.ge_pessoa d
             ON a.seqpessoa = d.seqpessoa
+       WHERE a.dtavda BETWEEN '01-aug-2023' and '31-aug-2024' AND
+             a.vlritem <= 20 AND
+             
+             
